bbb0cf8fc49a45e446a41c89dc87c9165d80914c
nickvibing <nicklauatan@gmail.com>
2023-07-24 12:51:58 -0700

    Starting Mini Game

---
+++ starting.py
@@ -0,0 +1,19 @@
+import time
+import os
+import random
+
+print ("Hello, We're going to be playing a game.")
+time.sleep (1)
+user_name = input ("What's your name?: ")
+time.sleep (1)
+print (f"Hey {user_name}. Here Are The Rules\n")
+print ("1. You only have 100 lives. Each time there's an error, you lose a life\n2. Don't Get Mad")
+time.sleep (1)
+os.system ("clear")
+def mathGame():
+    print ("Here's the first game")
+    time.sleep (1)
+    number_guess
+    number_guess = input ("A good old math game.\n Guess The Number out of 10,000!: ")
+    while True:
+        if number_guess
